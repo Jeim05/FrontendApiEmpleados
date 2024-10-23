b5e5bf19f5c701ed6affef896f6c3324ae924a05
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-22 21:15:14 -0600

    funcionalidades crud en el servicio

--- src/app/Services/empleado.service.ts
+++ src/app/Services/empleado.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { appsettings } from '../Settings/appsettings';
+import { Empleado } from '../Models/Empleado';
+import { ResponseApi } from '../Models/ResponsaApi';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadoService {
+  private http = inject(HttpClient);
+  private apiUrl: string = appsettings.apiUrl+"Empleado"
 
   constructor() { }
+
+  lista(){
+   return this.http.get<Empleado>(this.apiUrl);
+  }
+
+  obtener(id:number){
+    return this.http.get<Empleado>(`${this.apiUrl}/${id}`)
+  }
+
+  crear(objeto:Empleado){
+    return this.http.post<ResponseApi>(this.apiUrl,objeto);
+  }
+
+  editar(objeto:Empleado){
+    return this.http.put<ResponseApi>(this.apiUrl,objeto);
+  }
+
+  eliminar(id:number){
+    return this.http.delete<ResponseApi>(`${this.apiUrl}/${id}`)
+  }
 }
