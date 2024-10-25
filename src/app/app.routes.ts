d78baf3e3924f9f7ecc9559c27073b7e0e4d41bd
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-24 19:46:44 -0600

    Updated empleado component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { EmpleadoComponent } from './components/empleado/empleado.component';
 
 export const routes: Routes = [
   {path: "", component:HomeComponent},
-  {path:'empleado/:id',component:EmpleadoComponent},
 ];
