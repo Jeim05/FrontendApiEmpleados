452fd4270fefe4709433186043831fc879a25f7a
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-25 20:01:50 -0600

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { EmpleadoComponent } from './components/empleado/empleado.component';
+import { DepartamentoComponent } from './components/departamento/departamento.component';
 
 export const routes: Routes = [
-  {path: "", component:HomeComponent},
+  {path: '', component:HomeComponent},
+  {path: 'inicio', component:HomeComponent},
   {path:'empleado/:id',component:EmpleadoComponent},
+  {path: 'departamento', component:DepartamentoComponent}
 ];
