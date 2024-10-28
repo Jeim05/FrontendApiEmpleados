6659f141753487124df84ff33bad64d979ddcf48
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-27 20:42:53 -0600

    Servicio departamento llamar funciones

--- src/app/Services/departamento.service.ts
+++ src/app/Services/departamento.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { appsettings } from '../Settings/appsettings';
 import { Observable } from 'rxjs';
 import { Departamento } from '../Models/Departamento';
+import { ResponseApi } from '../Models/ResponsaApi';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +17,22 @@ export class DepartamentoService {
   lista():Observable<any>{
     return this.http.get<Departamento>(this.apiUrl);
   }
+
+  obtener(id:number){
+    return this.http.get<Departamento>(this.apiUrl);
+  }
+
+  crear(objeto:Departamento){
+    return this.http.post<ResponseApi>(this.apiUrl,objeto);
+  }
+
+  editar(objeto:Departamento){
+    return this.http.put<ResponseApi>(this.apiUrl,objeto);
+  }
+
+  eliminar(id:number){
+    return this.http.delete<ResponseApi>(`${this.apiUrl}/${id}`);
+  }
+
+
 }
