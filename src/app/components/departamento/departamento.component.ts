631c3be2757b879a6ffac311875a2c6b8242b56f
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-27 19:00:17 -0600

    Service Departamento

--- src/app/components/departamento/departamento.component.ts
+++ src/app/components/departamento/departamento.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { DepartamentoService } from '../../Services/departamento.service';
+import { Departamento } from '../../Models/Departamento';
 
 @Component({
   selector: 'app-departamento',
@@ -8,5 +12,31 @@ import { Component } from '@angular/core';
   styleUrl: './departamento.component.css'
 })
 export class DepartamentoComponent {
+  faPenToSquare = faPenToSquare; 
+  faTrashCan = faTrashCan;
+
+  private departamentoService = inject(DepartamentoService);
+  public listaDepartamentos: Departamento[] = [];
+
+  constructor(){
+    this.obtenerDepartamentos();
+  }
+
+  obtenerDepartamentos(){
+    this.departamentoService.lista().subscribe({
+      next: (data) => {
+        if (data.length > 0) {
+          this.listaDepartamentos = data;
+        }
+      },
+      error: (err) => {
+        console.log(err.message)
+      }
+    })
+  }
+
+  nuevo(){
+    
+  }
 
 }
